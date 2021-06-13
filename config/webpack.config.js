77a820d69a5839ac2d8fef5e66d8119f0b9ca605
TingjunWan <wtj9631@163.com>
2021-06-13 23:05:05 +0800

    搞定3个icon

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -367,6 +367,13 @@ module.exports = function (webpackEnv) {
           // match the requirements. When no loader matches it will fall
           // back to the "file" loader at the end of the loader list.
           oneOf: [
+            {
+              test: /\.svg$/,
+              use: [
+                {loader: 'svg-sprite-loader', options: {}},
+                {loader: 'svgo-loader', options: {}}
+              ]
+            },
             // TODO: Merge this config once `image/avif` is in the mime-db
             // https://github.com/jshttp/mime-db
             {
@@ -407,7 +414,7 @@ module.exports = function (webpackEnv) {
                     },
                   ],
                 ],
-                
+
                 plugins: [
                   [
                     require.resolve('babel-plugin-named-asset-import'),
@@ -452,7 +459,7 @@ module.exports = function (webpackEnv) {
                 cacheDirectory: true,
                 // See #6846 for context on why cacheCompression is disabled
                 cacheCompression: false,
-                
+
                 // Babel sourcemaps are needed for debugging into node_modules
                 // code.  Without the options below, debuggers like VSCode
                 // show incorrect code and set breakpoints on the wrong lines.
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,13 @@
-
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
+// import x from 'icons/money.svg'; //svg-loader--svg-sprite-loader
+//import y from 'icons/tag.svg'; //svg-loader--svg-sprite-loader
+
+require('icons/money.svg')
+require('icons/tag.svg')
+require('icons/chart.svg')
+// console.log(x,y);
 
 const NavWrapper = styled.nav`
 box-shadow:0 0 3px rgba(0,0,0,0.25);
@@ -11,7 +17,15 @@ line-height:24px;
   > li {
     width:33.3333%;
     text-align:center;
-    padding:16px;
+    display:flex;
+    padding:4px 0;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .icon {
+      width:24px;
+      height:24px;
+    }
   }
 }
 `
@@ -20,12 +34,21 @@ const X=()=>{
         <NavWrapper>
             <ul>
                 <li>
+                    <svg fill='red' className="icon">
+                        <use xlinkHref="#tag"></use>
+                    </svg>
                     <Link to="/tags">标签页</Link>
                 </li>
                 <li>
+                    <svg fill='red' className="icon">
+                        <use xlinkHref="#money"></use>
+                    </svg>
                     <Link to="/money">记账</Link>
                 </li>
                 <li>
+                    <svg fill='red' className="icon">
+                        <use xlinkHref="#chart"></use>
+                    </svg>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>
