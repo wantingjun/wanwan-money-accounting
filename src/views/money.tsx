09ce08448ebeee4caa73b0fe7d9a9b29c9f49906
TingjunWan <wtj9631@163.com>
2021-06-15 23:24:04 +0800

    money.tsx-键盘 section UI

--- src/views/money.tsx
+++ src/views/money.tsx
@@ -70,6 +70,63 @@ const  CategorySection = styled.section`
     }
 `
 const  NumberpageSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    > .output{
+        background: white;
+        font-size:36px;
+        line-height:72px;
+        text-align: right;
+        padding:0 16px;
+        box-shadow:inset 0 -5px 5px -5px rgba(0,0,0,0.25),inset 0 5px 5px -5px rgba(0,0,0,0.25);
+    }
+    > .pad{
+      > button{
+        font-size:18px;
+        float:left;
+        width:25%;
+        height:64px;
+        border:none;
+        &.ok{
+        height: 128px;
+        float: right;
+        }
+        &.dot{
+          width:50%;
+        }
+        &:nth-child(1){
+          background: #f2f2f2;
+        }
+        &:nth-child(2),
+        &:nth-child(5){
+          background: #e0e0e0;
+        }
+        
+        &:nth-child(3),
+        &:nth-child(6),
+        &:nth-child(9){
+          background: #d3d3d3;
+        }
+        &:nth-child(4),
+        &:nth-child(7),
+        &:nth-child(10){
+          background: #c1c1c1;
+        }
+        &:nth-child(8),
+        &:nth-child(11),
+        &:nth-child(13){
+          background: #b8b8b8;
+        }
+        &:nth-child(12){
+          background: #a9a9a9;
+        }
+        &:nth-child(14){
+          background: #9a9a9a;
+        }
+ 
+      }
+    }
+
 `
 
 function Money() {
@@ -97,8 +154,8 @@ function Money() {
                 </ul>
             </CategorySection>
             <NumberpageSection>
-                <div>100</div>
-                <div>
+                <div className="output">100</div>
+                <div className="pad clearfix">
                     <button>1</button>
                     <button>2</button>
                     <button>3</button>
@@ -110,9 +167,9 @@ function Money() {
                     <button>7</button>
                     <button>8</button>
                     <button>9</button>
-                    <button>ok</button>
-                    <button>0</button>
-                    <button>.</button>
+                    <button className="ok">ok</button>
+                    <button className="zero">0</button>
+                    <button className="dot">.</button>
 
                 </div>
             </NumberpageSection>
