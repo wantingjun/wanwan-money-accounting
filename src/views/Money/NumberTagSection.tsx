524559ed28207cb6101f26cf8561cfbda206fd5b
TingjunWan <wtj9631@163.com>
2021-06-17 20:58:57 +0800

    money.tsx - 数字面板 优化代码

--- src/views/Money/NumberTagSection.tsx
+++ src/views/Money/NumberTagSection.tsx
@@ -1,65 +1,8 @@
 import styled from "styled-components";
 import React, {useState} from "react";
+import Wrapper from "./NumberPagSection/Wrapper";
+import {generateOutput} from "./NumberPagSection/generateOutput";
 
-const  Wrapper = styled.section`
-    display: flex;
-    flex-direction: column;
-    > .output{
-        background: white;
-        font-size:36px;
-        line-height:72px;
-        text-align: right;
-        padding:0 16px;
-        box-shadow:inset 0 -5px 5px -5px rgba(0,0,0,0.25),inset 0 5px 5px -5px rgba(0,0,0,0.25);
-    }
-    > .pad{
-      > button{
-        font-size:18px;
-        float:left;
-        width:25%;
-        height:64px;
-        border:none;
-        &.ok{
-        height: 128px;
-        float: right;
-        }
-        &.dot{
-          width:50%;
-        }
-        &:nth-child(1){
-          background: #f2f2f2;
-        }
-        &:nth-child(2),
-        &:nth-child(5){
-          background: #e0e0e0;
-        }
-        
-        &:nth-child(3),
-        &:nth-child(6),
-        &:nth-child(9){
-          background: #d3d3d3;
-        }
-        &:nth-child(4),
-        &:nth-child(7),
-        &:nth-child(10){
-          background: #c1c1c1;
-        }
-        &:nth-child(8),
-        &:nth-child(11),
-        &:nth-child(13){
-          background: #b8b8b8;
-        }
-        &:nth-child(12){
-          background: #a9a9a9;
-        }
-        &:nth-child(14){
-          background: #9a9a9a;
-        }
- 
-      }
-    }
-
-`
 const NumberpageSection:React.FC =()=>{
     const [output,_setOutput]= useState('0');
     const setOutput=(output:string)=>{
@@ -71,53 +14,17 @@ const NumberpageSection:React.FC =()=>{
         }
         _setOutput(output)
     }
-    const onClickNumber=(number:number)=>{
-
-    }
     const onClickButtonWrapper=(e:React.MouseEvent)=>{
         const text = (e.target as HTMLButtonElement).textContent;
         if(text === null){return ;}
-        switch(text){
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                if(output === '0'){
-                    setOutput(text)
-                }else{
-                    setOutput(output+text)
-                }
-                console.log(text)
-                break;
-            case '.':
-                if(output.indexOf('.')>=0){
-                    return ;
-                }
-                setOutput(output+'.')
-                break;
-            case '删除':
-                if(output.length === 1){
-                    setOutput('0')
-                } else{
-                    setOutput(output.slice(0,-1))
-                }
-                console.log('delete')
-                break;
-            case '清空':
-                setOutput('')
-                console.log('清空')
-                break;
-            case 'ok':
-                console.log('确认')
-                break;
-
+        if(text === 'ok'){
+            //todo
+            return ;
         }
+        if('0123456789.'.split('').concat(['删除','清空']).indexOf(text)>=0){
+            setOutput(generateOutput(text,output))
+        }
+
     }
     return (
         <Wrapper>
