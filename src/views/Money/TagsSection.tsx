d14f59f580323285bbaff65e7ffe5d2e3ec15791
TingjunWan <wtj9631@163.com>
2021-06-27 11:27:36 +0800

    重构：将tags：string重构为tags:{id:number, name:string}

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -34,39 +34,39 @@ const  Wrapper = styled.section`
     }
 `
 type Props={
-    value:string[];
-    onChange:(selected:string[])=>void;
+    value:number[];
+    onChange:(selected:number[])=>void;
 }
 //类型React.FunctionComponent,间歇React.FC
 const  TagsSection:React.FunctionComponent<Props>=(props)=>{
+    console.log(props)
     const {tags, setTags} = useTags()
-    //const [selectedTags,setSelectedTags] = useState<string[]>([]);
-    const selectedTags= props.value;
+    const selectedTagIds= props.value;
     const onAddTag=()=>{
         const tagName = window.prompt('新标签的名称为');
         if(tagName !== null){
-            setTags([...tags,tagName])
+            setTags([...tags,{id:Math.random(),name:tagName}])
         }
     };
-    const onToggleTag=(tag:string)=>{
-        const index = selectedTags.indexOf(tag);
+    const onToggleTag=(tagId:number)=>{
+        const index = selectedTagIds.indexOf(tagId);
         if(index>=0){ //如果tag已被选中，就复制所有没有被选中的tag，作为新的selectedTags
             // 不能直接改selectedTags
-            props.onChange(selectedTags.filter(t=> t!==tag))
+            props.onChange(selectedTagIds.filter(t=> t!==tagId))
         }
         else{//否则，新的tag加上之前的selectedTags，组成新的selectedTags
-            props.onChange([...selectedTags,tag])
+            props.onChange([...selectedTagIds,tagId])
         }
     }
-    const getClass= (tag:string)=>selectedTags.indexOf(tag) >=0 ? 'selected' : '';
+    const getClass= (tagId:number)=>selectedTagIds.indexOf(tagId) >=0 ? 'selected' : '';
     return (
             <Wrapper>
                 <ol>
                     {tags.map(tag=>
-                    <li key={tag}
-                        onClick={()=>{onToggleTag(tag)}}
-                        className={getClass(tag)}>
-                         {tag}
+                    <li key={tag.id}
+                        onClick={()=>{onToggleTag(tag.id)}}
+                        className={getClass(tag.id)}>
+                         {tag.name}
                     </li>
                     )}
                 </ol>
--- src/views/money.tsx
+++ src/views/money.tsx
@@ -13,7 +13,7 @@ const MyLayout = styled(Layout)`
 type Category='-' |'+'
 function Money() {
     const [selected,setSelected] =useState({
-        tags:[] as string[],
+        tagIds:[] as number[],
         note:'',
         category:'-' as Category,
         amount:0
@@ -28,8 +28,7 @@ function Money() {
     // @ts-ignore
     return (
         <MyLayout className="hi">
-            {selected.amount}
-            <TagsSection value={selected.tags} onChange={(tags)=>onChange({tags})}/>
+            <TagsSection value={selected.tagIds} onChange={(tagIds)=>onChange({tagIds})}/>
             <NoteSection value={selected.note} onChange={(note)=>onChange({note})}/>
             <CategorySection value={selected.category} onChange={(category)=>onChange({category})}/>
             <NumberpageSection value={selected.amount}
