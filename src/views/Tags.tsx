cd987c7501e3c5d7ee321fd5377f262ce974d971
TingjunWan <wtj9631@163.com>
2021-06-27 18:09:30 +0800

    tags 删除id展示

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -35,7 +35,7 @@ function Tags() {
                 {tags.map(tag=>
                     <li key={tag.id}>
                         <Link to={'/tags/'+tag.id}>
-                            <span className="oneLine">{tag.name}:{tag.id}</span>
+                            <span className="oneLine">{tag.name}</span>
                             <Icon name="right"></Icon>
                         </Link>
 
