57d31abcd6dd13c5c2fc7b5498f03f8c29d97209
TingjunWan <wtj9631@163.com>
2021-06-27 10:49:46 +0800

    Tags list样式做完

--- src/views/tags.tsx
+++ src/views/tags.tsx
@@ -1,12 +1,57 @@
 import Layout from "../components/Layout";
 import React, {useState} from "react";
 import useTags from "useTags";
+import styled from "styled-components";
+import Icon from '../components/Icon'
 
+const Button=styled.button`
+    font-size:18px;
+    border:none;
+    padding:8px 12px;
+    border-radius:4px;
+    background: #767676;
+    color:white;
+`
+const Center=styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Space = styled.div`
+    height:16px;
+`
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+  > li{
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left:16px;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+ 
+  }
+`
 function Tags() {
    const {tags, setTags} = useTags()
     return (
         <Layout>
-            <h2>标签页</h2>
+            <TagList>
+                {tags.map(tag=>
+                    <li key={tag}>
+                        <span className="oneLine">{tag}</span>
+                        <Icon name="right"></Icon>
+                    </li>
+                )}
+            </TagList>
+            <Center>
+                <Space/>
+            <Button>新增标签</Button>
+                <Space/>
+            </Center>
         </Layout>
 
     );
