af66300f446bc1877487064a15bbe5a971f175bc
TingjunWan <wtj9631@163.com>
2021-06-28 00:15:58 +0800

    update title

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+yarn build &&
+cd build &&
+git init &&
+git add . &&
+git commit -m "deploy" &&
+git remote add origin git@github.com:wantingjun/wanwan-money-accounting-website.git &&
+git branch -M main &&
+git push -u origin main -f
+cd -
