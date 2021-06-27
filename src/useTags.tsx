02328e0184ccdaaf3d7c220f08786bd2aecdcf7e
TingjunWan <wtj9631@163.com>
2021-06-27 13:46:03 +0800

    编辑tag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -10,10 +10,29 @@ const defaultTags = [
 const useTags = ()=>{
     const [tags,setTags] = useState<{id:number;name:string}[]>(defaultTags);
     const findTag = (id:number) =>tags.filter(tag=>tag.id === id)[0]
+    const findTagIndex = (id:number)=>{
+        let result = -1;
+        for(let i =0;i<tags.length;i++){
+            if(tags[i].id == id){
+                result = i;
+                break;
+            }
+        }
+        return result
+    };
+    const updateTag =(id:number,obj:{name:string})=>{
+        const index = findTagIndex(id);
+        // 神拷贝tags
+        const TagsClone = JSON.parse(JSON.stringify(tags));
+        // tagsClone的第index个删掉，换成id:number,obj:{name:string}
+        TagsClone.splice(index,1,{id:id,name:obj.name});
+        setTags(TagsClone)
+    };
     return {
         tags:tags,
         setTags:setTags,
-        findTag:findTag
+        findTag:findTag,
+        updateTag:updateTag
     }
 }
 export default useTags;
